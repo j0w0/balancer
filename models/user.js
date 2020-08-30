c970b6ade1fdce8e58781391cc13e104b2f6a7d0
Josh Woodcock <josh@j0w0.com>
2020-08-29 22:40:45 -0500

    Create Schemas and Models

---
+++ models/user.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const billSchema = Schema({
+    name: String,
+    dueDate: Date,
+    autoPay: Boolean,
+    autoPayAccount: String,
+    total: {
+        type: Number,
+        default: 0.00
+    },
+    url: String,
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: categorySchema
+    }
+}, {
+    timestamps: true
+});
+
+const lineItemSchema = Schema({
+    bill: {
+        type: Schema.Types.ObjectId,
+        ref: billSchema
+    },
+    paymentAmount: {
+        type: Number,
+        default: 0.00
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: categorySchema
+    }
+}, {
+    timestamps: true
+});
+
+const budgetSchema = Schema({
+    name: String,
+    bills: [ billSchema ],
+    paySchedule: String
+}, {
+    timestamps: true
+});
+
+const balanceSchema = Schema({
+    date: Date,
+    lineItems: [ lineItemSchema ],
+    notes: String
+}, {
+    timestamps: true
+});
+
+const categorySchema = Schema({
+    name: String,
+}, {
+    timestamps: true
+});
+
+const userSchema = Schema({
+    name: String,
+    email: String,
+    googleId: String,
+    budget: [ budgetSchema ],
+    balances: [ balanceSchema ],
+    categories: [ categorySchema ]
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // window.open("https://www.google.com")
   res.render('index', { title: 'Express' });
 });
 
